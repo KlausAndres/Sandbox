4e53f227587c2bf9df70336800d3a845f64432fb
Klaus <28318034+KlausAnd@users.noreply.github.com>
2023-05-13 10:03:40 +0200

    Commit

--- Project/Scripts/Main.gd
+++ Project/Scripts/Main.gd
@@ -19,3 +19,14 @@ func _on_ButtonLeft_pressed():
 func _on_ButtonRight_pressed():
 	pic_index += 1
 	_update_picture()
+
+
+func _on_ButtonDownload_pressed():
+	# var image = Image.new()
+	var image = Image.load_from_file("res://assets/download/Bild1.JPG")
+	var buf = image.save_jpg_to_buffer()
+	
+	
+#	var image = Image.load_from_path(preload("res://assets/download/Bild1.JPG"))
+#	var buf = image.save_jpg_to_buffer()
+#	JavaScript.download_buffer(buf, 'NewName.jpg')
